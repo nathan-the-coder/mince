9901a4c987739a9092731517f1ca496a2fce39af
nathan-the-coder <nathanielramos726@gmail.com>
2022-07-09 14:55:09 +0800

    quiet's git and infom the user while cloning

--- scripts/git.py
+++ scripts/git.py
@@ -5,12 +5,14 @@ import time
 
 def clone(src, dest):
     try:
+        log.Log(1, "Cloning...")
         if os.system(f"git clone -q {src} {dest}") != 0:
             _in = input("You I remove the directory? ")
             if _in == "yes":
                 log.Log(2, "Removing directory!")
                 time.sleep(3)
                 shop.shutil.rmtree(dest)
+                log.Log(1, "Cloning...")
                 os.system(f"git clone -q {src} {dest}")
             elif _in == "no":
                 log.Log(3, "Cannot Proceed: Directory Exists")
