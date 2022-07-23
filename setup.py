9de69a085b868d6b79dfbe1d78923220bff64bb7
nathan-the-coder <nathanielramos726@gmail.com>
2022-07-23 20:47:03 +0800

    changes setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,24 @@ from scripts import log, shop
 
 # Variables to store directories used by the conditionals
 localDir = "/home/nathan/.local/"
-pythonDir = localDir + "lib/python3.10/site-packages/"
-ql = localDir + 'bin/ql'
-qls = pythonDir + "qls/"
+
+# check for the python local path if it is 3.10 or 3.9
+if os.path.isDir(localDir + "lib/python3.10/site-packages/"):
+    pythonDir = localDir + "lib/python3.10/site-packages/"
+else:
+    pythonDir = localDir + "lib/python3.9/site-packages/"
+
+# snak installation dirs
+snak = localDir + 'bin/snak'
+snak_ext = pythonDir + "snak_ext/"
 
 # Setup the syntax files for specified text editor
 if log.sys.argv[1] == "editor":
-    # This will install ql syntax file to neovim
+    # This will install snak syntax file to neovim
     if log.sys.argv[2] == "neovim": 
         try:
 
-            shop.cp("editor/ql.vim", "/home/nathan/.config/nvim/syntax/")
+            shop.cp("editor/snak.vim", "/home/nathan/.config/nvim/syntax/")
         except FileNotFoundError:
             shop.mkdir("/home/nathan/.config/nvim/syntax")
 
@@ -31,21 +38,20 @@ if log.sys.argv[1] == "editor":
 # Install ql, ql source and scripts to localDir
 elif log.sys.argv[1] == "install":
     # QL 
-    shop.install(755, "ql.py", ql)
+    shop.install(755, "snak.py", snak)
     
     # QL source directory
-    shop.install(0, "qls", qls)
+    shop.install(0, "snak-ext", snake_ext)
 
     # Scripts for the other functionalities of ql
     shop.install(755, "scripts/log.py", pythonDir+"log.py")
     shop.install(755, "scripts/shop.py", pythonDir+"shop.py")
-    shop.install(755, "scripts/git.py", pythonDir+"git.py")
 
 # Remove folders/files if exists
 elif log.sys.argv[1] == 'clean':
     try:
         # using the shop (Shell Operations) module
-        shop.rem(ql)
-        shop.rem(qls+"*")
+        shop.rem(snak)
+        shop.rem(snake_ext+"*")
     except FileNotFoundError:
         pass
